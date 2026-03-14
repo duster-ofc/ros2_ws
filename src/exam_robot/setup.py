197f56774e19f7931903c6b88238b328789749c0
duster-ofc <niktick2076@gmail.com>
2026-03-14 20:34:04 +0400

    Update setup.py

--- src/exam_robot/setup.py
+++ src/exam_robot/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob  # ← Добавлено
 
 package_name = 'exam_robot'
 
@@ -6,24 +8,23 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='student',
     maintainer_email='student@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
     entry_points={
         'console_scripts': [
+            'battery_node = exam_robot.battery_node:main',
         ],
     },
 )
